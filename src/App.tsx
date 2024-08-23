aa5caf2f66eb6e0e50258c89fdf2446e49e12634
Ian Myjer <imyjer@manifold.ai>
2024-08-23 10:26:18 -0700

    penny

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Penny from "./assets/penny.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={Penny} className="logo penny" />
+      </div>
+      Welcome to Ian Myjer Dot Com!
+    </>
+  );
+}
+
+export default App;

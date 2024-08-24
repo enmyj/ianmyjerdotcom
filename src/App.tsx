0000459a0af7d022dab0e484ffb87724a8a7809e
Ian Myjer <imyjer@gmail.com>
2024-08-24 14:44:08 -0700

    material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import Framily from "./assets/framily.jpg";
 import "./App.css";
+import { Box, Typography } from "@mui/material";
 
 function App() {
   return (
-    <>
-      <div className="image-container">
-        <img src={Framily} alt="Framily" className="centered-image" />
-      </div>
-      <div>Welcome to Ian Myjer Dot Com!</div>
-    </>
+    <Box max-width="1280px" padding="10px" textAlign="center">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        width="100%"
+        overflow="hidden"
+        paddingBottom="10px"
+      >
+        <Box
+          component="img"
+          sx={{
+            maxWidth: "500px",
+            width: "90%",
+            height: "auto",
+            objectFit: "scale-down",
+          }}
+          alt="Centered Image"
+          src={Framily}
+        />
+      </Box>
+      <Typography fontSize="20px">Welcome to Ian Myjer dot Com!</Typography>
+    </Box>
   );
 }
 

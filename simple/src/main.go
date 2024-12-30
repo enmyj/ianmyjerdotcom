842bd31c7d06529be033b9c056f3f9aa8da64dc5
enmyj <imyjer@manifold.ai>
2024-12-29 21:16:19 -0800

    attempt fiber templates (#1)
    * attempt fiber templates
    
    * fixes

--- simple/src/main.go
+++ simple/src/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
 
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	app.Use(logger.New())
+	app.Use(cache.New(cache.Config{
+		Expiration:   24 * time.Hour,
+		CacheControl: true,
+	}))
 
-	app.Static("/", "./static")
+	app.Static("/favicon.ico", "./static/images/favicon.png")
+	app.Static("/static", "./static")
 
-	app.Listen(":80")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{}, "layouts/main")
+	})
+
+	app.Get("/about.html", func(c *fiber.Ctx) error {
+		return c.Render("about", fiber.Map{}, "layouts/main")
+	})
+
+	log.Fatal(app.Listen(":80"))
 }
